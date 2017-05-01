6e21c863322da4159d188574c6669d89a5e10841
omkarvijay5 <omkarvijay5@gmail.com>
2017-05-01 18:40:20 +0530

    separate out list, create and put, post, delete in two dfferent end points

--- bookmark/urls.py
+++ bookmark/urls.py
@@ -5,10 +5,11 @@
 from django.conf.urls import url
 
 # third party imports
-from bookmark.api import BookmarkApi
+from bookmark.api import BookmarkApi, BookmarkCreateListApi
 
 
 urlpatterns = [
     # url(r'^(?P<url>(.*))/$', BookmarkApi.as_view(), name='bookmark_api')
-    url(r'^bookmarks/$', BookmarkApi.as_view(), name='bookmark_api')
+    url(r'^bookmarks/$', BookmarkCreateListApi.as_view(), name='bookmark_list_create_api'),
+    url(r'^bookmarks/(?P<pk>[0-9]+)/$', BookmarkApi.as_view(), name='bookmark_detail_api')
 ]
