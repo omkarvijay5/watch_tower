f2b3a5b8fc2e17a1b10b8ae5eb18f6051d9f4efa
omkarvijay5 <omkarvijay5@gmail.com>
2017-05-01 17:18:09 +0530

    created bookmark admin

--- bookmark/admin.py
+++ bookmark/admin.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+# django imports
 from django.contrib import admin
 
+# third party imports
+from bookmark.models import Bookmark
 # Register your models here.
+
+@admin.register(Bookmark)
+class BookmarkAdmin(admin.ModelAdmin):
+    # list_display = ('name', 'url')
+    pass
