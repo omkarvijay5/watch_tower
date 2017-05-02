c762631098c12b457171a72e67d4ad62776ebbd9
omkarvijay5 <omkarvijay5@gmail.com>
2017-05-02 14:03:45 +0530

    filtering logic search bookmarks

--- static/js/controllers/BookmarkListController.js
+++ static/js/controllers/BookmarkListController.js
@@ -12,3 +12,33 @@ function BookmarkListController($scope, $resource, BookmarkListService) {
 angular
   .module('bookmark')
   .controller('BookmarkListController', BookmarkListController)
+
+
+angular.module('bookmark').filter('searchBookmark', function(){
+
+	// All filters must return a function. The first parameter
+	// is the data that is to be filtered, and the second is an
+	// argument that may be passed with a colon (searchFor:searchString)
+
+	return function(arr, searchString){
+
+		if(!searchString){
+			return arr;
+		}
+
+		var result = [];
+		searchString = searchString.toLowerCase();
+
+		// Using the forEach helper method to loop through the array
+		angular.forEach(arr, function(item){
+
+			if(item.name.toLowerCase().indexOf(searchString) !== -1){
+				result.push(item);
+			}
+
+		});
+
+		return result;
+	};
+
+});
